6c4d75d81b33d1cdd93c1355dacb30879ed391e7
itsnilskerwer <uougk@student.kit.edu>
2024-09-01 18:20:34 +0200

    reafctored with own template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	/* site: 'https://noartroom.github.io/blog', */
+	site: 'https://ethereum-viewer.github.io/',
+	/* trailingSlash: 'ignore', */
+/* 	trailingSlash: 'never',
+ */	/* base: '/blog',  */
+	integrations: [sitemap()]
+});
